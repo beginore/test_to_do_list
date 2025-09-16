9fb6f1799eda4669104155db95e5fcbaf2a35335
Shynggys <cterekbaev@gmail.com>
2025-09-16 06:58:09 +0500

    init tables

---
+++ 1.initial_tables.sql
@@ -0,0 +1,7 @@
+CREATE TABLE todos (
+                       id SERIAL PRIMARY KEY,
+                       title TEXT NOT NULL,
+                       completed BOOLEAN DEFAULT FALSE,
+                       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                       completed_at TIMESTAMP NULL
+);
